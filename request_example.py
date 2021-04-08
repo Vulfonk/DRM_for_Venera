1e8d4caaa553663cdf38718cef8aaa1d0e1818bd
Vulfonk <podkalyuk-dima@bk.ru>
2021-04-09 02:30:55 +0500

    -исправили файл с сокетами, в котором хранилось не то

--- request_example.py
+++ request_example.py
@@ -1,27 +1,12 @@
-import os
-import hashlib
-from glob import glob
-from functools import partial
-
-def get_hashes(dirr, ext):
-    files = os.listdir(dirr)
-    directoryPath  = dirr + "\*."
-    fileExtensions = ext
-    listOfFiles    = []
-    for extension in fileExtensions:
-        listOfFiles.extend( glob( directoryPath + extension ))
-    return _get_hashes(listOfFiles)
-
-def _get_hashes(files):
-    res = {}
-    for file in files:
-        res[str(file)] = {}
-        with open(str(file), "rb") as f:      
-            sha256 = hashlib.sha256()
-            for buf in iter(partial(f.read, 2048), b''):
-                sha256.update(buf)
-            res[str(file)]["sha256"] = sha256.hexdigest()
-    return res
-    
-
-get_hashes("C:\diplom", ["pdf", "doc", "xlsx", "rtf"])
+from socket import *
+import time
+s = socket(AF_INET, SOCK_STREAM)
+s.bind(('',10000))
+s.listen((10))
+while True :
+    client, addr = s.accept()
+    print(client)
+    print("Запрос на соединение от %s" % str(addr))
+    timestr=time.ctime(time.time())+"\n"
+    client.send(timestr.encode('utf-8'))
+    client.close()
