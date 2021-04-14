85ecf79018ddc290ad2765c4e84c442c64bf92ef
Vulfonk <podkalyuk-dima@bk.ru>
2021-04-14 16:46:30 +0500

    -обернули обращения к БД в класс - обернули вычисление хэша директории в класс

--- hash_example.py
+++ hash_example.py
@@ -4,51 +4,23 @@ import sqlite3
 from glob import glob
 from functools import partial
 
-def get_hashes(dirr, ext, res = {}):
-    files = []
-    for current_folder in list(os.walk(dirr)):
-        for current_file in current_folder[2]:
-            path_to_file = current_folder[0] + "\\" + current_file
-            if os.path.splitext(path_to_file)[1][1:] in ext:
-                get_hash(path_to_file, res)
-    return res
+class DictHashesClass(object):
 
-def get_hash(file, res):
-    res[str(file)] = {}
-    with open(str(file), "rb") as f:      
-        sha256 = hashlib.sha256()
-        for buf in iter(partial(f.read, 2048), b''):
-            sha256.update(buf)
-        res[str(file)]["sha256"] = sha256.hexdigest()
-    return res
-
-
-def add_etalon(folder_path, ext):
-    db = sqlite3.connect('DRM-etalon.db')
-    dict_hashes  = get_hashes(folder_path, ext)
-    cur = db.cursor()
-
-    cur.execute(
-        f"""INSERT INTO Etalons (AbsolutePath) 
-        VALUES ('{folder_path}');""")
-
-    select_query = f"""SELECT Id
-            FROM Etalons
-            WHERE AbsolutePath = '{folder_path}'
-            LIMIT  1"""
-            
-    cur.execute(select_query)
-    result = cur.fetchone()[0]
-
-    for pair in dict_hashes:
-        relative_path = '.' + pair.replace(folder_path, '', 1)
-        hash_sha = dict_hashes[pair]['sha256']
-
-        insert_query =f"""INSERT INTO FilesHash (RelativePath, Hash, EtalonId) 
-                VALUES ('{relative_path}', '{hash_sha}','{result}')
-                """ 
+    def __init__(self, dirr, ext, res = {}):
+        files = []
+        for current_folder in list(os.walk(dirr)):
+            for current_file in current_folder[2]:
+                path_to_file = current_folder[0] + "\\" + current_file
+                if os.path.splitext(path_to_file)[1][1:] in ext:
+                    self.get_hash(path_to_file, res)
+        self.dict_hashes = res
+        self.dirr_path = dirr
         
-        cur.executescript(insert_query)
-    db.commit()
-
-add_etalon(r"C:\Users\1\Desktop\HUDs", ["py", "doc", "xlsx", "rtf", "txt", "xml"])
+    def get_hash(self, file, res):
+        res[str(file)] = {}
+        with open(str(file), "rb") as f:      
+            sha256 = hashlib.sha256()
+            for buf in iter(partial(f.read, 2048), b''):
+                sha256.update(buf)
+            res[str(file)]["sha256"] = sha256.hexdigest()
+        return res
